98d79ca6cb0dcf6dbc099ab17df350019ab30716
dhanushrajgp <gdhanush68@gmail.com>
2024-07-20 17:22:26 +0530

    updated CORS to handle request from any origin

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,13 @@ mod state;
 mod to_do;
 mod views;
 use actix_service::Service;
+use actix_cors::Cors;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let cors = Cors::default().allow_any_origin()
+        .allow_any_method()
+        .allow_any_header();
         let app = App::new()
             .wrap_fn(|req, srv| {
                 println!("{:?}", req);
@@ -18,7 +22,7 @@ async fn main() -> std::io::Result<()> {
                     Ok(result)
                 }
             })
-            .configure(views::views_factory);
+            .configure(views::views_factory).wrap(cors);
         return app;
     })
     .bind("127.0.0.1:8000")?
