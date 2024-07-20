392b56e3e8116827886896f1ce9337e0498afab0
dhanushrajgp <gdhanush68@gmail.com>
2024-07-20 12:13:11 +0530

    created css styling for to_do and reusable function to interlink  html and css file to main.html aloong with Api call from javascript

--- src/views/app/items.rs
+++ src/views/app/items.rs
@@ -1,9 +1,20 @@
-use super::content_loader::read_file;
+use super::content_loader::{read_file,add_component};
+
 use actix_web::HttpResponse;
 pub async fn items() -> HttpResponse {
-    let mut html_data = read_file("C:/todo_app_webservice/src/templates/main.html");
-    let javascript_data = read_file("C:/todo_app_webservice/src/javascript/main.js");
+    let mut html_data = read_file("./src/templates/main.html");
+    let javascript_data = read_file("./src/javascript/main.js");
+    let css_data: String = read_file(
+        "./src/css/main.css");
+    let base_css_data: String = read_file(
+        "./src/css/base.css");
     html_data = html_data.replace("{{JAVASCRIPT}}", &javascript_data);
+    html_data = html_data.replace("{{CSS}}", 
+    &css_data);
+    html_data = html_data.replace("{{BASE_CSS}}", 
+    &base_css_data);
+    html_data = add_component(String::from("header"), 
+    html_data);
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(html_data)
