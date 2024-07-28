edbba2aaf041afc8bb3f8ebc2868e9609e7dad02
dhanushrajgp <gdhanush68@gmail.com>
2024-07-28 09:03:25 +0530

    implemented api version check as a middleware before routing into any views

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[macro_use] extern crate diesel;
 extern crate dotenv;
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer,HttpResponse};
 mod json_serialization;
 mod jwt;
 mod models;
@@ -11,8 +11,13 @@ mod database;
 use actix_service::Service;
 use actix_cors::Cors;
 mod schema;
+use futures::future::{ok,Either};
+
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    const ALLOWED_VERSION: &'static str= include_str!("./output_data.txt");
     HttpServer::new(|| {
         let cors = Cors::default().allow_any_origin()
         .allow_any_method()
@@ -20,9 +25,22 @@ async fn main() -> std::io::Result<()> {
         let app = App::new()
             .wrap_fn(|req, srv| {
                 println!("{:?}", req);
-                let future = srv.call(req);
-                async {
-                    let result = future.await?;
+                let passed:bool;
+                if *&req.path().contains(&format!("/{}/",ALLOWED_VERSION)){
+                    passed = true;
+                }
+                else{
+                    passed = false;
+                }
+                let end_result= match passed {
+                  true => { Either::Left(srv.call(req))}
+                false=>{
+                    let resp = HttpResponse::NotImplemented().body(format!("only {} API is supported",ALLOWED_VERSION));
+                    Either::Right(ok(req.into_response(resp).map_into_boxed_body()))
+                }
+                    };
+                async move{
+                    let result = end_result.await?;
                     Ok(result)
                 }
             })
